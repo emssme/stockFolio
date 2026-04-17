ded96abba0759b6c8a08ed8761e330b02578ec8a
emssme <bk75121@gmail.com>
2026-04-17 16:38:52 +0900

    [feat] ApiResponse/ErrorResponse 공통 응답 구조 추가

--- stockfolio/src/main/java/com/stockfolio/global/response/ApiResponse.java
+++ stockfolio/src/main/java/com/stockfolio/global/response/ApiResponse.java
@@ -1,9 +1,33 @@
 package com.stockfolio.global.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.stockfolio.global.exception.ErrorCode;
+
+import lombok.*;
+
 // success: true/false
 // data: 성공 시 실제 데이터
 // error: 실패 시 에러 정보
 
-public class ApiResponse {
+@Getter
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
     
+    private final boolean success;
+    private final T data;
+    private final ErrorResponse error;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true, null, null);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return new ApiResponse<>(false, null, new ErrorResponse(errorCode));
+    }
 }
