ba56b8754c3ca8c6c5ca0970ce7d8b6b312d79a1
emssme <bk75121@gmail.com>
2026-04-20 17:11:01 +0900

    [feat] 회원가입 API 구현

---
+++ stockfolio/src/main/java/com/stockfolio/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.stockfolio.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stockfolio.global.response.ApiResponse;
+import com.stockfolio.user.dto.SignUpRequest;
+import com.stockfolio.user.dto.SignUpResponse;
+import com.stockfolio.user.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+    
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<SignUpResponse>> signup(@RequestBody @Valid SignUpRequest req) {
+        SignUpResponse res = userService.signup(req);
+        return ResponseEntity.status(201).body(ApiResponse.success(res));
+    }
+}
