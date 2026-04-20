ecba16cb818bd9901cc5397a9ffcbd8908b613f3
emssme <bk75121@gmail.com>
2026-04-20 14:04:37 +0900

    [feat] GlobalException 및 ExceptionHandler 추가

--- stockfolio/src/main/java/com/stockfolio/global/exception/GlobalException.java
+++ stockfolio/src/main/java/com/stockfolio/global/exception/GlobalException.java
@@ -1,5 +1,14 @@
 package com.stockfolio.global.exception;
 
-public class GlobalException {
-    
+public class GlobalException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
 }
